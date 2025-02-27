daddbe2aa88a35edb81ad5bd811587d6be2f012a
William Martinsson <william.martinsson@madepeople.se>
2025-02-27 22:34:10 +0100

    fix live preview

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,8 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {presentationTool} from 'sanity/presentation'
+import {locations} from './presentation/locations'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,20 @@ export default defineConfig({
   projectId: '6ut6pd3v',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    presentationTool({
+      previewUrl: {
+        origin: 'http://localhost:3000',
+        preview: '/',
+        previewMode: {
+          // This should match the route you created earlier
+          enable: '/resource/preview',
+        },
+      },
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
